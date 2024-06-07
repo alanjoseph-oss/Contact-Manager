207ec4e245fb26194dfd8dc640234193f3c1ff57
alanjoseph-oss <150259468+alanjoseph-oss@users.noreply.github.com>
2024-06-07 11:04:14 +0530

    Updates

---
+++ precache-manifest.995e52cc1bec0b31b9c6f1b650c67eff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e9ce013199cc706899aafc2125f749",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "cdc434655cf56c1e9eea",
+    "url": "/Contact-Manager/static/css/main.c7e5f565.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/Contact-Manager/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Contact-Manager/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "cdc434655cf56c1e9eea",
+    "url": "/Contact-Manager/static/js/main.57b1da2f.chunk.js"
+  },
+  {
+    "revision": "32524840be08acfd0ed0",
+    "url": "/Contact-Manager/static/js/runtime~main.ee4b505a.js"
+  }
+]);
